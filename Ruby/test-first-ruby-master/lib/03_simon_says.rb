a1ab0b97fcdd108ce6229461389cccf37b1d60bb
Newtonius <newtonius235@gmail.com>
2016-07-09 17:22:39 -0400

    We're gettin' there

---
+++ Ruby/test-first-ruby-master/lib/03_simon_says.rb
@@ -0,0 +1,21 @@
+def echo(word)
+  "#{word}"
+end
+
+def shout(word)
+  "#{word}".upcase
+end
+
+def repeat(word, rep = 1)
+  
+end
+
+def start_of_word
+
+end
+
+def first_word
+end
+
+def titleize
+end
