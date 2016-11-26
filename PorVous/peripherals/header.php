c8e46e506d1cec11c99a7c9e260e452cbc648513
Newtonius <newtonius235@gmail.com>
2016-11-26 14:01:25 -0500

    yeah

--- PorVous/peripherals/header.php
+++ PorVous/peripherals/header.php
@@ -1,16 +1,16 @@
 <style type="text/css">
-        <?php 
+        <?php
             $r=rand(0,255);
             $g=rand(0,255);
             $b=rand(0,255);
         ?>
     #mainNavBar a:hover, visited li {
-        -webkit-text-shadow: 2px 2px 5px #ffffff; 
+        -webkit-text-shadow: 2px 2px 5px #ffffff;
         text-shadow: 2px 2px 5px #ffffff;
         background:rgba(
             <?php echo $r ?>,
             <?php echo $g ?>,
-            <?php echo $b ?>,             
+            <?php echo $b ?>,
             0.75);
         -webkit-transform:scale(1.15)translateY(5px);
         transform:scale(1.15)translateY(5px);
@@ -25,26 +25,28 @@
 <header>
     <div>
         <ul id="mainNavBar">
-			<a href="http://Porvous.com/default.php" target="_self">
+          <!--
+			       <a href="http://Porvous.com/Home.php" target="_self">
                 <li>
                     <p>
                         Home
                     </p>
                 </li>
-            </a>
-            <?php
-                $navTab = array("Gallery","Travel","Contact");
-                for($i=1; $i<=count($navTab); $i++):    
-            ?>
-            <a href="http://Porvous.com/<?php echo $navTab[$i-1] ?>.php" target="_self">
+              </a>
+            -->
+              <?php
+                $navTab = array("Home", "Gallery","Travel","Contact");
+                for($i=1; $i<=count($navTab); $i++):
+                  ?>
+              <a href="http://Porvous.com/<?php echo $navTab[$i-1] ?>.php" target="_self">
                 <li>
-                    
+
                      <!-- <img src="http://Porvous.com/GFX/siteGFX/<?php echo $navTab[$i-1] ?>-icon.png" align="left" width="30" height="30" alt="" /> -->
                     <p>
                         <?php echo $navTab[$i-1] ?>
                     </p>
                 </li>
-            </a>
+              </a>
             <?php endfor; ?>
         </ul>
     </div>
--- Ruby/test-first-ruby-master/lib/04_pig_latin.rb
+++ Ruby/test-first-ruby-master/lib/04_pig_latin.rb
@@ -1,5 +1,44 @@
-def translate(word)
 
-  
+def translate(input)
 
+	x= input.split(" ")
+	l=x.size
+	sentence=[]
+	if  l > 1
+		sentence=x.collect { |word|  translate(word)}
+		return sentence.join(" ")
+	end
+
+	if vowel?(input)==true
+		input = input+"ay"
+	else
+	 consonant(input)
+	end
+end
+
+def vowel?(word)
+# Determine if the word starts with a vowel
+ vowels = ["a","e","i","o","u"]
+	if vowels.include?(word[0])
+		true
+	else
+		false
+	end
+end
+
+def consonant(word)
+  # Determine if the word has consonants at the 1, 2, or 3 letter. Or if 2 and 3 letter are sequentially q and u
+	letter_1 = word[0]
+	letter_2 = word[1]
+	letter_3 = word[2]
+	if vowel?(letter_1)==false && ((vowel?(letter_2)==false && vowel?(letter_3)==false) || (letter_2=="q" && letter_3=="u") )
+		new_string = word[0..2]
+		word[3..-1] + new_string + "ay"
+
+	elsif (vowel?(letter_1)==false && vowel?(letter_2)==false) || (letter_1=="q" && letter_2=="u")
+			new_string = word[0..1]
+			word[2..-1] + new_string + "ay"
+	else
+		word=word[1..-1] + letter_1 + "ay"
+	end
 end
