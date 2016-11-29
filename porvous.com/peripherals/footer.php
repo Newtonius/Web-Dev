a488094cb3c553070b88490519fee84db0547283
Newtonius <newtonius235@gmail.com>
2016-11-28 21:20:51 -0500

    A whole new world

--- porvous.com/peripherals/footer.php
+++
@@ -1,9 +0,0 @@
-	</div>
-</div>
-<footer id="footer">
-    <p>
-		A site for you, my schmetterling!<br/>
-        And look! a link to your 
-		<a href="http://onskedrom.weebly.com/" target="_blank">blog!</a> ;p
-    </p>
-</footer>
